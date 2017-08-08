304f35950e2c2c05994a6a3fe7a467e1399fd7b9
Tom Brooks <tom.brooks@tbrooks-ltm.internal.salesforce.com>
2017-08-07 21:20:03 -0400

    Removed extra add plugin vars and cleaned up Java classes

--- src/android/MCPushCordovaApplication.java
+++ src/android/MCPushCordovaApplication.java
@@ -1,236 +1,40 @@
 package com.salesforce.mcpush;
 
 import android.app.Application;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.Log;
-import android.content.pm.ApplicationInfo;
 import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.preference.PreferenceManager;
-import android.content.SharedPreferences;
-import java.util.LinkedHashSet;
-import org.json.JSONObject;
-import java.util.Set;
-import android.os.Bundle;
-import org.json.JSONException;
-import android.support.v4.app.NotificationManagerCompat;
+import android.support.annotation.CallSuper;
 
+import com.salesforce.marketingcloud.InitializationStatus;
 import com.salesforce.marketingcloud.MCLogListener;
 import com.salesforce.marketingcloud.MarketingCloudConfig;
 import com.salesforce.marketingcloud.MarketingCloudSdk;
-import com.salesforce.marketingcloud.InitializationStatus;
-import com.salesforce.marketingcloud.registration.Registration;
-import com.salesforce.marketingcloud.registration.RegistrationManager;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
 
-import java.util.Locale;
+public class MCPushCordovaApplication extends Application {
 
-public class MCPushCordovaApplication extends Application implements MCLogListener,RegistrationManager.RegistrationEventListener {
+    private static final String TAG = "~#MCPushCordovaApp";
 
-	private static final String TAG = CONSTS.LOG_TAG;
+    private static Context context;
 
-	public static boolean isNotificationEnabledStatus;
-	private String devMCAppId, devAccessToken, devGcmSenderId, prodMCAppId, prodAccessToken, prodGcmSenderId;
-	private Boolean analyticsEnabled, piAnalyticsEnabled, locationEnabled, proximityEnabled, cloudPagesEnabled, overrideNotifications;
-	private static Context context;
+    public static Context getAppContext() {
+        return context;
+    }
 
-	/**
-	 * The onCreate() method initialize your app.
-	 * <p/>
-	 * It registers the application to listen for events posted to a private communication bus
-	 * by the SDK and calls `MCPush.readyAimFire` to configures the SDK to point to the correct code
-	 * application and to initialize the MCPush, according to the constants defined before.
-	 * <p/>
-	 * When ReadyAimFire() is called for the first time for a device, it will get a device token
-	 * from Google and send to the MarketingCloud.
-	 * <p/>
-	 * In MCPush.readyAimFire() you must set several parameters:
-	 * <ul>
-	 * <li>
-	 * AppId and AccessToken: these values are taken from the Marketing Cloud definition for your app.
-	 * </li>
-	 * <li>
-	 * GcmSenderId for the push notifications: this value is taken from the Google API console.
-	 * </li>
-	 * <li>
-	 * You also set whether you enable LocationManager, CloudPages, and Analytics.
-	 * </li>
-	 * </ul>
-	 * <p/>
-	 * <p/>
-	 * The application keys are stored in a separate file (secrets.xml) in order to provide
-	 * centralized access to these keys and to ensure you use the appropriate keys when
-	 * compiling the test and production versions.
-	 **/
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		MCPushCordovaApplication.context = getApplicationContext();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
 
-		/**
-		 * Register the application to listen for events posted to a private communication bus
-		 * by the SDK.
-		 */
-
-		try {
-
-			//Get MCSDK configuration from secrets.xml resource
-			devMCAppId = this.getResources().getString(this.getResources().getIdentifier("MCPUSH_DEV_APPID", "string", this.getPackageName()));
-			if (devMCAppId == null || devMCAppId.isEmpty()) throw new RuntimeException("Unable to find MCPUSH_DEV_APPID in secrets.xml");
-			devAccessToken = this.getResources().getString(this.getResources().getIdentifier("MCPUSH_DEV_ACCESSTOKEN", "string", this.getPackageName()));
-			if (devAccessToken == null || devAccessToken.isEmpty()) throw new RuntimeException("Unable to find MCPUSH_DEV_ACCESSTOKEN in secrets.xml");
-			devGcmSenderId = this.getResources().getString(this.getResources().getIdentifier("MCPUSH_DEV_GCMSENDERID", "string", this.getPackageName()));
-			if (devGcmSenderId == null || devGcmSenderId.isEmpty()) throw new RuntimeException("Unable to find MCPUSH_DEV_GCMSENDERID in secrets.xml");
-			prodMCAppId = this.getResources().getString(this.getResources().getIdentifier("MCPUSH_PROD_APPID", "string", this.getPackageName()));
-			if (prodMCAppId == null || prodMCAppId.isEmpty()) throw new RuntimeException("Unable to find MCPUSH_PROD_APPID in secrets.xml");
-			prodAccessToken = this.getResources().getString(this.getResources().getIdentifier("MCPUSH_PROD_ACCESSTOKEN", "string", this.getPackageName()));
-			if (prodAccessToken == null || prodAccessToken.isEmpty()) throw new RuntimeException("Unable to find MCPUSH_PROD_ACCESSTOKEN in secrets.xml");
-			prodGcmSenderId = this.getResources().getString(this.getResources().getIdentifier("MCPUSH_PROD_GCMSENDERID", "string", this.getPackageName()));
-			if (prodGcmSenderId == null || prodGcmSenderId.isEmpty()) throw new RuntimeException("Unable to find MCPUSH_PROD_GCMSENDERID in secrets.xml");
-			analyticsEnabled = this.getResources().getBoolean(this.getResources().getIdentifier("MCPUSH_ANALYTICS_ENABLED", "bool", this.getPackageName()));
-			if (analyticsEnabled == null) throw new RuntimeException("Unable to find MCPUSH_ANALYTICS_ENABLED in secrets.xml");
-			piAnalyticsEnabled = this.getResources().getBoolean(this.getResources().getIdentifier("MCPUSH_WAMA_ENABLED", "bool", this.getPackageName()));
-			if (piAnalyticsEnabled == null) throw new RuntimeException("Unable to find MCPUSH_WAMA_ENABLED in secrets.xml");
-			locationEnabled = this.getResources().getBoolean(this.getResources().getIdentifier("MCPUSH_LOCATION_ENABLED", "bool", this.getPackageName()));
-			if (locationEnabled == null) throw new RuntimeException("Unable to find MCPUSH_LOCATION_ENABLED in secrets.xml");
-			proximityEnabled = this.getResources().getBoolean(this.getResources().getIdentifier("MCPUSH_PROXIMITY_ENABLED", "bool", this.getPackageName()));
-			if (proximityEnabled == null) throw new RuntimeException("Unable to find MCPUSH_PROXIMITY_ENABLED in secrets.xml");
-			cloudPagesEnabled = this.getResources().getBoolean(this.getResources().getIdentifier("MCPUSH_CLOUDPAGES_ENABLED", "bool", this.getPackageName()));
-			if (cloudPagesEnabled == null) throw new RuntimeException("Unable to find MCPUSH_CLOUDPAGES_ENABLED in secrets.xml");
-			overrideNotifications = this.getResources().getBoolean(this.getResources().getIdentifier("MCPUSH_OVERRIDE_NTFN_ENABLED", "bool", this.getPackageName()));
-			if (overrideNotifications == null) throw new RuntimeException("Unable to find MCPUSH_OVERRIDE_NTFN_ENABLED in secrets.xml");
-
-
-
-			String mcAppId;
-			String accessToken;
-			String gcmSenderId;
-			boolean isDebuggable = (0 != (this.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE));
-			if (isDebuggable) {
-				//MCPush.setLogLevel(Log.DEBUG);
-				mcAppId = devMCAppId;
-				accessToken = devAccessToken;
-				gcmSenderId = devGcmSenderId;
-			} else {
-				mcAppId = prodMCAppId;
-				accessToken = prodAccessToken;
-				gcmSenderId = prodGcmSenderId;
-			}
-
-			MarketingCloudSdk.setLogLevel(MCLogListener.VERBOSE);
-			MarketingCloudSdk.setLogListener(new MCLogListener.AndroidLogListener());
-			//if (pushConfig != null) {
-				MarketingCloudSdk.init(this, MarketingCloudConfig.builder()
-		        .setApplicationId(mcAppId)
-		        .setAccessToken(accessToken)
-		        .setGcmSenderId(gcmSenderId)
-		        //Enable any other feature desired.
-		        .build(), new MarketingCloudSdk.InitializationListener() {
-			      @Override public void complete(InitializationStatus status) {
-			        if (status.isUsable()) {
-			          if (status.status() == InitializationStatus.Status.COMPLETED_WITH_DEGRADED_FUNCTIONALITY) {
-			            // While the SDK is usable, something happened during init that you should address.
-			            // This could include:
-			            if (status.locationsError()) {
-			                final GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-			                Log.i(TAG, String.format(Locale.ENGLISH, "Google Play Services Availability: %s", googleApiAvailability.getErrorString(status.locationPlayServicesStatus())));
-			                if (googleApiAvailability.isUserResolvableError(status.locationPlayServicesStatus())) {
-			                    googleApiAvailability.showErrorNotification(MCPushCordovaApplication.this, status.locationPlayServicesStatus());
-			                }
-			            }
-			          }
-			        } else {
-			          //Something went wrong with init that makes the SDK unusable.
-
-			        }
-
-					MarketingCloudSdk.requestSdk(new MarketingCloudSdk.WhenReadyListener() {
-						@Override
-						public void ready(MarketingCloudSdk sdk) {
-						  sdk.getRegistrationManager().registerForRegistrationEvents(MCPushCordovaApplication.this);
-						}
-					});
-			      }
-			    });
-			/*} else {
-				Log.e(TAG, "MCPushConfig is null");
-			}*/
-		} catch (Exception e) {
-			Log.e(TAG, e.getMessage(), e);
-		}
-
-		//cloudPageListAdapter = new CDVCloudPageListAdapter(getApplicationContext());
-	}
-
-	/**
-	 * Listens for Registrations
-	 * <p/>
-	 * This method is one of several methods to log notifications when an event occurs in the SDK.
-	 * Different attributes indicate which event has occurred.
-	 * <p/>
-	 * RegistrationEvent will be triggered when the SDK receives the response from the
-	 * registration as triggered by the com.google.android.c2dm.intent.REGISTRATION intent.
-	 */
-	@Override
-	public void onRegistrationReceived(@NonNull Registration registration) {
-		MarketingCloudSdk.getInstance().getAnalyticsManager().trackPageView("data://RegistrationEvent", "Registration Event Completed");
-		if (MarketingCloudSdk.getLogLevel() <= Log.DEBUG) {
-			Log.d(TAG, "Marketing Cloud update occurred.");
-			Log.d(TAG, "Device ID:" + registration.deviceId());
-			Log.d(TAG, "Device Token:" + registration.systemToken());
-			Log.d(TAG, "Language: " + registration.locale());
-			Log.d(TAG, String.format("Last sent: %1$d", System.currentTimeMillis()));
-		}
-	}
-
-	@Override
-	public void out(int severity, String tag, String message, @Nullable Throwable throwable) {
-		/*
-		 * Using this method you can interact with SDK log output.
-		 * Severity is populated with log levels like Log.VERBOSE, Log.INFO etc.
-		 * Message, is populated with the actual log output text.
-		 * Tag, is a free form string representing the log tag you've selected.
-		 * Finally, the optional Throwable Throwable represents a thrown exception.
-		 */
-
-		/*
-		 * Assuming you have crashytics enabled for your app, the following code would send
-		 * log data to Crashytics in the event that the log's severity is ERROR or ASSERT
-		 */
-
-		if (throwable != null) {
-			// We have an exception to log:
-			// Commenting out all references to Crashlytics.
-			// Crashlytics.logException(throwable);
-		}
-
-		switch (severity) {
-		case Log.ERROR:
-			Log.e(tag, message);
-			// Crashlytics.log(severity, tag, message);
-			break;
-		case Log.ASSERT:
-			Log.wtf(tag, message);
-			// Crashlytics.log(severity, tag, message);
-			try {
-				// If we're logging a failed ASSERT, also grab the getSDKState() data and log that as well
-				/*MarketingCloudSdk cloudSdk = MarketingCloudSdk.getInstance();
-				Log.v("SDKState Information", cloudSdk.getSdkState());*/
-				// Crashlytics.log(MCPush.getInstance().getSDKState());
-			} catch (Exception e) {
-				//Log.v("ErrorGettingSDKState", etException.getMessage());
-				// Crashlytics.log(String.format(Locale.ENGLISH, "Error Getting SDK State: %s", etException.getMessage()));
-			}
-			break;
-		default:
-			Log.v(tag, message);
-		}
-	}
-
-	public static Context getAppContext() {
-        return MCPushCordovaApplication.context;
+        MarketingCloudSdk.init(this, MarketingCloudConfig.builder()
+                .setApplicationId(getString(R.string.MCPUSH_PROD_APPID))
+                .setAccessToken(getString(R.string.MCPUSH_PROD_ACCESSTOKEN))
+                .setGcmSenderId(getString(R.string.MCPUSH_PROD_GCMSENDERID))
+                .build(), new MarketingCloudSdk.InitializationListener() {
+            @Override
+            public void complete(InitializationStatus status) {
+                if (!status.isUsable()) {
+                    // failed log
+                }
+            }
+        });
     }
 }
--- src/android/MCPushCordovaPlugin.java
+++ src/android/MCPushCordovaPlugin.java
@@ -1,159 +1,130 @@
 package com.salesforce.mcpush;
- 
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+import com.salesforce.marketingcloud.MarketingCloudSdk;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.json.JSONObject;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-
-import com.salesforce.marketingcloud.messages.push.PushMessageManager;
-import com.salesforce.marketingcloud.MarketingCloudSdk;
-import com.salesforce.marketingcloud.registration.RegistrationManager;
-
-import android.content.Context;
-import android.app.Application;
-import android.app.Activity;
-import android.support.v4.app.NotificationManagerCompat;
-import android.preference.PreferenceManager;
-import android.content.SharedPreferences;
-
-import android.util.Log;
-import org.apache.cordova.*;
-
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.ArrayList;
+import java.util.Locale;
 
 public class MCPushCordovaPlugin extends CordovaPlugin {
-	
-	private static final String TAG = CONSTS.LOG_TAG;
+    private static final String TAG = "~#MCPushCordovaPlugin";
+    private static final String ACTION_GET_SDK_VERSION_NAME = "getSdkVersionName";
+    private static final String ACTION_GET_SYSTEM_TOKEN = "getSystemToken";
+    private static final String ACTION_ENABLE_PUSH = "enablePush";
+    private static final String ACTION_DISABLE_PUSH = "disablePush";
+    private static final String ACTION_IS_PUSH_ENABLED = "isPushEnabled";
+    private static final String ACTION_IS_NOTIFICATION_ENABLED = "isNotificationEnabled";
+    private static final String ACTION_SET_NOTIFICATION_HANDLER = "setNotificationHandler";
+    private static CallbackContext notificationCallback;
 
-	public static final String ACTION_GET_SDK_VERSION_NAME = "getSdkVersionName";
-	public static final String ACTION_GET_SYSTEM_TOKEN = "getSystemToken";
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.v(TAG, String.format(Locale.ENGLISH, "EXECUTING ACTION: %s", action));
+        switch (action) {
+            case ACTION_GET_SDK_VERSION_NAME:
+                return handleGetSdkVersionName(callbackContext);
+            case ACTION_GET_SYSTEM_TOKEN:
+                return handleGetSystemToken(callbackContext);
+            case ACTION_IS_NOTIFICATION_ENABLED:
+                return handleIsNotificationEnabled(callbackContext);
+            case ACTION_ENABLE_PUSH:
+                return handleEnablePush(callbackContext);
+            case ACTION_DISABLE_PUSH:
+                return handleDisablePush(callbackContext);
+            case ACTION_IS_PUSH_ENABLED:
+                return handleIsPushEnabled(callbackContext);
+            case ACTION_SET_NOTIFICATION_HANDLER:
+                return handleSetNotificationHandler(callbackContext);
+            default:
+                callbackContext.error("Invalid action");
+                return false;
+        }
+    }
 
-	public static final String ACTION_ENABLE_PUSH = "enablePush";
-	public static final String ACTION_DISABLE_PUSH = "disablePush";
-	public static final String ACTION_IS_PUSH_ENABLED = "isPushEnabled";
-	public static final String ACTION_IS_NOTIFICATION_ENABLED = "isNotificationEnabled";
-	public static final String ACTION_SET_NOTIFICATION_HANDLER = "setNotificationHandler";
+    private boolean handleSetNotificationHandler(CallbackContext callbackContext) {
+        if (notificationCallback != null) {
+            callbackContext.error("Notification handler already running.");
+            return true;
+        }
+        notificationCallback = callbackContext;
+        Log.d(TAG, "EXECUTING ACTION_SET_NOTIFICATION_HANDLER");
+        // Don't return any result now, since status results will be sent when events come in from broadcast receiver
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+        return true;
+    }
 
-	private static CallbackContext notificationCallback;
+    private boolean handleIsPushEnabled(CallbackContext callbackContext) {
+        Boolean isPushEnabled;
+        try {
+            isPushEnabled = MarketingCloudSdk.getInstance().getPushMessageManager().isPushEnabled();
+        } catch (Exception e) {
+            return caughtException(callbackContext, e);
+        }
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, isPushEnabled));
+        return true;
+    }
 
-  /**
-   * Main CordovaPlugin execute function
-   */
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	  
-	  Log.d(TAG, "EXECUTING " + action);	
-		
-		try {
-			if (ACTION_GET_SDK_VERSION_NAME.equals(action)) {
+    private boolean handleDisablePush(CallbackContext callbackContext) {
+        try {
+            MarketingCloudSdk.getInstance().getPushMessageManager().disablePush();
+        } catch (Exception e) {
+            return caughtException(callbackContext, e);
+        }
+        callbackContext.success();
+        return true;
+    }
 
-				String versionName;
-				try {
-					versionName = MarketingCloudSdk.getInstance().getSdkVersionName();
-				} catch (Exception e) {
-					Log.e(TAG, e.getMessage(), e);
-					callbackContext.error(e.getMessage());
-					return false;          	
-				}
+    private boolean handleEnablePush(CallbackContext callbackContext) {
+        try {
+            MarketingCloudSdk.getInstance().getPushMessageManager().enablePush();
+        } catch (Exception e) {
+            return caughtException(callbackContext, e);
+        }
+        callbackContext.success();
+        return true;
+    }
 
-				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, versionName));
-				return true; 	
+    private boolean handleIsNotificationEnabled(CallbackContext callbackContext) {
+        boolean notificationStatus;
+        try {
+            notificationStatus = NotificationManagerCompat.from(MCPushCordovaApplication.getAppContext()).areNotificationsEnabled();
+        } catch (Exception e) {
+            return caughtException(callbackContext, e);
+        }
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, notificationStatus));
+        return true;
+    }
 
-			} else if (ACTION_GET_SYSTEM_TOKEN.equals(action)) { 
-				String systemToken;
-				try {
-						systemToken = MarketingCloudSdk.getInstance().getRegistrationManager().getSystemToken();
-				} catch (Exception e) {
-					Log.e(TAG, e.getMessage(), e);
-				    callbackContext.error(e.getMessage());
-				    return false;          	
-				}
+    private boolean handleGetSystemToken(CallbackContext callbackContext) {
+        String systemToken;
+        try {
+            systemToken = MarketingCloudSdk.getInstance().getRegistrationManager().getSystemToken();
+        } catch (Exception e) {
+            return caughtException(callbackContext, e);
+        }
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, systemToken));
+        return true;
+    }
 
-				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, systemToken));
-				return true;
+    private boolean handleGetSdkVersionName(CallbackContext callbackContext) {
+        String versionName;
+        try {
+            versionName = MarketingCloudSdk.getSdkVersionName();
+        } catch (Exception e) {
+            return caughtException(callbackContext, e);
+        }
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, versionName));
+        return true;
+    }
 
-		    } else if(ACTION_IS_NOTIFICATION_ENABLED.equals(action)){
-		  		boolean notificationStatus;
-		  		try {
-		  			Context ctx = MCPushCordovaApplication.getAppContext();
-
-		  			//isNotificationEnabledStatus = NotificationManagerCompat.from(ctx).areNotificationsEnabled();
-		  			//notificationStatus = MCPushCordovaApplication.isNotificationEnabledStatus;
-		  			notificationStatus = NotificationManagerCompat.from(ctx).areNotificationsEnabled();
-		  		}catch(Exception e){
-		  			Log.e(TAG, e.getMessage(), e);
-		    		callbackContext.error(e.getMessage());
-		    		return false; 
-		  		}
-		  		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, notificationStatus));
-		      	return true;
-
-			}else if (ACTION_ENABLE_PUSH.equals(action)) {
-
-				try {
-					MarketingCloudSdk.getInstance().getPushMessageManager().enablePush();
-				} catch (Exception e) {
-					Log.e(TAG, e.getMessage(), e);
-					callbackContext.error(e.getMessage());
-					return false;          	
-				}
-
-				callbackContext.success();
-				return true;
-
-			} else if (ACTION_DISABLE_PUSH.equals(action)) {
-
-	  			try {
-	  				MarketingCloudSdk.getInstance().getPushMessageManager().disablePush();
-		      	} catch (Exception e) {
-		        	Log.e(TAG, e.getMessage(), e);
-				    	callbackContext.error(e.getMessage());
-				    	return false;          	
-		      	}
-
-				callbackContext.success();
-				return true;	  
-
-			} else if (ACTION_IS_PUSH_ENABLED.equals(action)) {
-
-				Boolean isPushEnabled;
-				try {
-					isPushEnabled = MarketingCloudSdk.getInstance().getPushMessageManager().isPushEnabled();
-				} catch (Exception e) {
-					Log.e(TAG, e.getMessage(), e);
-					callbackContext.error(e.getMessage());
-					return false;          	
-				}
-
-				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, isPushEnabled));
-				return true; 	  
-
-			} else if (ACTION_SET_NOTIFICATION_HANDLER.equals(action)) {
-
-				if (notificationCallback != null) {
-				    callbackContext.error( "Notification handler already running.");
-				    return true;
-				}
-				notificationCallback = callbackContext;
-				Log.d(TAG, "EXECUTING ACTION_SET_NOTIFICATION_HANDLER");
-				// Don't return any result now, since status results will be sent when events come in from broadcast receiver
-				PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-				pluginResult.setKeepCallback(true);
-				callbackContext.sendPluginResult(pluginResult);
-				return true;
-
-			} else {
-				//INVALID ACTION
-				callbackContext.error("Invalid action");
-				return false;
-			} 
-		} catch(Exception e) {
-		System.err.println("Exception: " + e.getMessage());
-		callbackContext.error(e.getMessage());
-		return false;
-		}
-	}
+    private boolean caughtException(CallbackContext callbackContext, Exception e) {
+        Log.e(TAG, e.getMessage(), e);
+        callbackContext.error(e.getMessage());
+        return false;
+    }
 }
