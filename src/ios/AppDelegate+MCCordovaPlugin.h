55b62adee874c92a34209bd2282ccd5afcddb363
Seth Clark <seth.clark@salesforce.com>
2018-08-29 09:19:05 -0400

    nuke everything

--- src/ios/AppDelegate+MCCordovaPlugin.h
+++
@@ -1,9 +0,0 @@
-#import "AppDelegate.h"
-#import "MarketingCloudSDK/MarketingCloudSDK.h"
-
-
-@interface AppDelegate (MCCordovaPlugin) <UNUserNotificationCenterDelegate>
-	- (BOOL)application : (UIApplication *)application shouldInitETSDKWithOptions : (NSDictionary *)launchOptions;
-@end
-
-        
