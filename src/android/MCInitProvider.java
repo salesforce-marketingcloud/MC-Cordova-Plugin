f68a1586b7db66dbe517e5681871205bcfefab30
Seth Clark <seth.clark@salesforce.com>
2018-08-29 14:09:10 -0400

    add version to registration

--- src/android/MCInitProvider.java
+++ src/android/MCInitProvider.java
@@ -8,19 +8,23 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.salesforce.marketingcloud.InitializationStatus;
+import com.salesforce.marketingcloud.MarketingCloudConfig;
 import com.salesforce.marketingcloud.MarketingCloudSdk;
+import com.salesforce.marketingcloud.registration.RegistrationManager;
+import java.util.Set;
 
-public class MCInitProvider extends ContentProvider {
+public class MCInitProvider extends ContentProvider
+    implements MarketingCloudSdk.InitializationListener {
+
+  private static final String CURRENT_CORDOVA_VERSION_NAME = "MC_Cordova_v1.1.0";
 
   @Override public boolean onCreate() {
     Context ctx = getContext();
     if (ctx != null) {
-      MarketingCloudSdk.init(ctx, MCSdkConfig.prepareConfigBuilder(ctx).build(ctx),
-          new MarketingCloudSdk.InitializationListener() {
-            @Override public void complete(@NonNull InitializationStatus initializationStatus) {
-
-            }
-          });
+      MarketingCloudConfig.Builder builder = MCSdkConfig.prepareConfigBuilder(ctx);
+      if (builder != null) {
+        MarketingCloudSdk.init(ctx, builder.build(ctx), this);
+      }
     }
     return false;
   }
@@ -48,5 +52,25 @@ public class MCInitProvider extends ContentProvider {
   public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection,
       @Nullable String[] selectionArgs) {
     return 0;
+  }
+
+  @Override public void complete(@NonNull InitializationStatus status) {
+    if (status.isUsable()) {
+      MarketingCloudSdk.requestSdk(new MarketingCloudSdk.WhenReadyListener() {
+        @Override public void ready(@NonNull MarketingCloudSdk marketingCloudSdk) {
+          RegistrationManager registrationManager = marketingCloudSdk.getRegistrationManager();
+          RegistrationManager.Editor registrationEditor = registrationManager.edit();
+          Set<String> tags = registrationManager.getTags();
+          if (!tags.isEmpty()) {
+            for (String tag : tags) {
+              if (!tag.equals(CURRENT_CORDOVA_VERSION_NAME) && tag.startsWith("MC_Cordova_v")) {
+                registrationEditor.removeTags(tag);
+              }
+            }
+          }
+          registrationEditor.addTags(CURRENT_CORDOVA_VERSION_NAME).commit();
+        }
+      });
+    }
   }
 }
