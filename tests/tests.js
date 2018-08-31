cdec6348936bd7bdc22ec3d560da3b026e9da2c7
Seth Clark <seth.clark@salesforce.com>
2018-08-31 09:25:10 -0400

    js tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,69 +1,80 @@
-// exports.defineAutoTests = function () {
-//   // describe('MCCordovaPlugin', function () {
 
-//   //   it('MCCordovaPlugin', function () {
-//   //     expect(MCCordovaPlugin).toBeDefined()
-//   //   });
-//   // });
-
-
-// }
 
 exports.defineAutoTests = function () {
-  // describe('MCCordovaPlugin', function () {
-  //   it('should exist', function () {
-  //     expect(MCCordovaPlugin).toBeDefined();
-  //   });
 
-  describe("A suite", function () {
-    it("contains spec with an expectation", function () {
-      expect(true).toBe(true);
+  describe('MCCordovaPlugin structure', function () {
+    it('plugin.spec.1 should exist', function () {
+      expect(MCCordovaPlugin).toBeDefined();
     });
 
+    it('plugin.spec.2 should contain a isPushEnabled function', function () {
+      expect(typeof MCCordovaPlugin.isPushEnabled).toBeDefined();
+      expect(typeof MCCordovaPlugin.isPushEnabled === 'function').toBe(true);
+    });
 
-    // it('should contain a getPicture function', function () {
-    //   expect(navigator.camera.getPicture).toBeDefined();
-    //   expect(typeof navigator.camera.getPicture === 'function').toBe(true);
-    // });
+    it('plugin.spec.3 should contain a enablePush function', function () {
+      expect(typeof MCCordovaPlugin.enablePush).toBeDefined();
+      expect(typeof MCCordovaPlugin.enablePush === 'function').toBe(true);
+    });
+
+    it('plugin.spec.4 should contain a disablePush function', function () {
+      expect(typeof MCCordovaPlugin.disablePush).toBeDefined();
+      expect(typeof MCCordovaPlugin.disablePush === 'function').toBe(true);
+    });
+
+    it('plugin.spec.5 should contain a getSystemToken function', function () {
+      expect(typeof MCCordovaPlugin.getSystemToken).toBeDefined();
+      expect(typeof MCCordovaPlugin.getSystemToken === 'function').toBe(true);
+    });
+
+    it('plugin.spec.6 should contain a getAttributes function', function () {
+      expect(typeof MCCordovaPlugin.getAttributes).toBeDefined();
+      expect(typeof MCCordovaPlugin.getAttributes === 'function').toBe(true);
+    });
+
+    it('plugin.spec.7 should contain a setAttribute function', function () {
+      expect(typeof MCCordovaPlugin.setAttribute).toBeDefined();
+      expect(typeof MCCordovaPlugin.setAttribute === 'function').toBe(true);
+    });
+
+    it('plugin.spec.8 should contain a clearAttribute function', function () {
+      expect(typeof MCCordovaPlugin.clearAttribute).toBeDefined();
+      expect(typeof MCCordovaPlugin.clearAttribute === 'function').toBe(true);
+    });
+
+    it('plugin.spec.9 should contain a addTag function', function () {
+      expect(typeof MCCordovaPlugin.addTag).toBeDefined();
+      expect(typeof MCCordovaPlugin.addTag === 'function').toBe(true);
+    });
+
+    it('plugin.spec.10 should contain a removeTag function', function () {
+      expect(typeof MCCordovaPlugin.removeTag).toBeDefined();
+      expect(typeof MCCordovaPlugin.removeTag === 'function').toBe(true);
+    });
+
+    it('plugin.spec.11 should contain a getTags function', function () {
+      expect(typeof MCCordovaPlugin.getTags).toBeDefined();
+      expect(typeof MCCordovaPlugin.getTags === 'function').toBe(true);
+    });
+
+    it('plugin.spec.12 should contain a setContactKey function', function () {
+      expect(typeof MCCordovaPlugin.setContactKey).toBeDefined();
+      expect(typeof MCCordovaPlugin.setContactKey === 'function').toBe(true);
+    });
+
+    it('plugin.spec.13 should contain a getContactKey function', function () {
+      expect(typeof MCCordovaPlugin.getContactKey).toBeDefined();
+      expect(typeof MCCordovaPlugin.getContactKey === 'function').toBe(true);
+    });
+
+    it('plugin.spec.14 should contain a enableVerboseLogging function', function () {
+      expect(typeof MCCordovaPlugin.enableVerboseLogging).toBeDefined();
+      expect(typeof MCCordovaPlugin.enableVerboseLogging === 'function').toBe(true);
+    });
+
+    it('plugin.spec.15 should contain a disableVerboseLogging function', function () {
+      expect(typeof MCCordovaPlugin.disableVerboseLogging).toBeDefined();
+      expect(typeof MCCordovaPlugin.disableVerboseLogging === 'function').toBe(true);
+    });
   });
-
-  // describe('Camera Constants (window.Camera + navigator.camera)', function () {
-  //     it('camera.spec.1 window.Camera should exist', function () {
-  //         expect(window.Camera).toBeDefined();
-  //     });
-
-  //     it('camera.spec.2 should contain three DestinationType constants', function () {
-  //         expect(Camera.DestinationType.DATA_URL).toBe(0);
-  //         expect(Camera.DestinationType.FILE_URI).toBe(1);
-  //         expect(Camera.DestinationType.NATIVE_URI).toBe(2);
-  //         expect(navigator.camera.DestinationType.DATA_URL).toBe(0);
-  //         expect(navigator.camera.DestinationType.FILE_URI).toBe(1);
-  //         expect(navigator.camera.DestinationType.NATIVE_URI).toBe(2);
-  //     });
-
-  //     it('camera.spec.3 should contain two EncodingType constants', function () {
-  //         expect(Camera.EncodingType.JPEG).toBe(0);
-  //         expect(Camera.EncodingType.PNG).toBe(1);
-  //         expect(navigator.camera.EncodingType.JPEG).toBe(0);
-  //         expect(navigator.camera.EncodingType.PNG).toBe(1);
-  //     });
-
-  //     it('camera.spec.4 should contain three MediaType constants', function () {
-  //         expect(Camera.MediaType.PICTURE).toBe(0);
-  //         expect(Camera.MediaType.VIDEO).toBe(1);
-  //         expect(Camera.MediaType.ALLMEDIA).toBe(2);
-  //         expect(navigator.camera.MediaType.PICTURE).toBe(0);
-  //         expect(navigator.camera.MediaType.VIDEO).toBe(1);
-  //         expect(navigator.camera.MediaType.ALLMEDIA).toBe(2);
-  //     });
-
-  //     it('camera.spec.5 should contain three PictureSourceType constants', function () {
-  //         expect(Camera.PictureSourceType.PHOTOLIBRARY).toBe(0);
-  //         expect(Camera.PictureSourceType.CAMERA).toBe(1);
-  //         expect(Camera.PictureSourceType.SAVEDPHOTOALBUM).toBe(2);
-  //         expect(navigator.camera.PictureSourceType.PHOTOLIBRARY).toBe(0);
-  //         expect(navigator.camera.PictureSourceType.CAMERA).toBe(1);
-  //         expect(navigator.camera.PictureSourceType.SAVEDPHOTOALBUM).toBe(2);
-  //     });
-  // });
 };
