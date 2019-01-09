a8e2f67a0fc7cf8580ebff33542581f701176b0a
Seth Clark <seth.clark@salesforce.com>
2019-01-09 15:12:28 -0500

    adding coding standard

--- src/android/MCSdkConfig.java
+++ src/android/MCSdkConfig.java
@@ -1,19 +1,19 @@
 /**
  * Copyright 2018 Salesforce, Inc
- *
+ * <p>
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met:
- *
+ * <p>
  * 1. Redistributions of source code must retain the above copyright notice, this list of
  * conditions and the following disclaimer.
- *
+ * <p>
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
  * conditions and the following disclaimer in the documentation and/or other materials provided
  * with the distribution.
- *
+ * <p>
  * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
  * endorse or promote products derived from this software without specific prior written permission.
- *
+ * <p>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
@@ -33,92 +33,94 @@ import android.util.Log;
 import com.google.firebase.FirebaseApp;
 import com.salesforce.marketingcloud.MarketingCloudConfig;
 import com.salesforce.marketingcloud.notifications.NotificationCustomizationOptions;
-import java.io.IOException;
-import java.util.Locale;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.Locale;
 
 import static com.salesforce.marketingcloud.cordova.MCCordovaPlugin.TAG;
 
 public class MCSdkConfig {
 
-  private static final String CONFIG_PREFIX = "com.salesforce.marketingcloud.";
+    private static final String CONFIG_PREFIX = "com.salesforce.marketingcloud.";
 
-  private MCSdkConfig() {
-  }
-
-  @Nullable public static MarketingCloudConfig.Builder prepareConfigBuilder(Context context) {
-    Resources res = context.getResources();
-    int configId = res.getIdentifier("config", "xml", context.getPackageName());
-
-    if (configId == 0) {
-      return null;
+    private MCSdkConfig() {
     }
 
-    XmlResourceParser parser = res.getXml(configId);
+    @Nullable
+    public static MarketingCloudConfig.Builder prepareConfigBuilder(Context context) {
+        Resources res = context.getResources();
+        int configId = res.getIdentifier("config", "xml", context.getPackageName());
 
-    return parseConfig(context, parser);
-  }
-
-  static MarketingCloudConfig.Builder parseConfig(Context context, XmlPullParser parser) {
-    MarketingCloudConfig.Builder builder = MarketingCloudConfig.builder();
-    boolean senderIdSet = false;
-    try {
-      while (parser.next() != XmlPullParser.END_DOCUMENT) {
-        if (parser.getEventType() != XmlPullParser.START_TAG || !"preference".equals(
-            parser.getName())) {
-          continue;
+        if (configId == 0) {
+            return null;
         }
 
-        String key = parser.getAttributeValue(null, "name");
-        String val = parser.getAttributeValue(null, "value");
+        XmlResourceParser parser = res.getXml(configId);
 
-        if (key != null && val != null) {
-          key = key.toLowerCase(Locale.US);
+        return parseConfig(context, parser);
+    }
 
-          switch (key) {
-            case CONFIG_PREFIX + "app_id":
-              builder.setApplicationId(val);
-              break;
-            case CONFIG_PREFIX + "access_token":
-              builder.setAccessToken(val);
-              break;
-            case CONFIG_PREFIX + "sender_id":
-              builder.setSenderId(val);
-              senderIdSet = true;
-              break;
-            case CONFIG_PREFIX + "analytics":
-              builder.setAnalyticsEnabled("true".equalsIgnoreCase(val));
-              break;
-            case CONFIG_PREFIX + "notification_small_icon":
-              int notifId =
-                  context.getResources().getIdentifier(val, "drawable", context.getPackageName());
-              if (notifId != 0) {
-                builder.setNotificationCustomizationOptions(
-                    NotificationCustomizationOptions.create(notifId));
-              }
-              break;
-            case CONFIG_PREFIX + "tenant_specific_endpoint":
-              builder.setMarketingCloudServerUrl(val);
-              break;
-          }
+    static MarketingCloudConfig.Builder parseConfig(Context context, XmlPullParser parser) {
+        MarketingCloudConfig.Builder builder = MarketingCloudConfig.builder();
+        boolean senderIdSet = false;
+        try {
+            while (parser.next() != XmlPullParser.END_DOCUMENT) {
+                if (parser.getEventType() != XmlPullParser.START_TAG || !"preference".equals(
+                        parser.getName())) {
+                    continue;
+                }
+
+                String key = parser.getAttributeValue(null, "name");
+                String val = parser.getAttributeValue(null, "value");
+
+                if (key != null && val != null) {
+                    key = key.toLowerCase(Locale.US);
+
+                    switch (key) {
+                        case CONFIG_PREFIX + "app_id":
+                            builder.setApplicationId(val);
+                            break;
+                        case CONFIG_PREFIX + "access_token":
+                            builder.setAccessToken(val);
+                            break;
+                        case CONFIG_PREFIX + "sender_id":
+                            builder.setSenderId(val);
+                            senderIdSet = true;
+                            break;
+                        case CONFIG_PREFIX + "analytics":
+                            builder.setAnalyticsEnabled("true".equalsIgnoreCase(val));
+                            break;
+                        case CONFIG_PREFIX + "notification_small_icon":
+                            int notifId =
+                                    context.getResources().getIdentifier(val, "drawable", context.getPackageName());
+                            if (notifId != 0) {
+                                builder.setNotificationCustomizationOptions(
+                                        NotificationCustomizationOptions.create(notifId));
+                            }
+                            break;
+                        case CONFIG_PREFIX + "tenant_specific_endpoint":
+                            builder.setMarketingCloudServerUrl(val);
+                            break;
+                    }
+                }
+            }
+        } catch (XmlPullParserException e) {
+            Log.e(TAG, "Unable to read config.xml.", e);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Unable to open config.xml.", ioe);
         }
-      }
-    } catch (XmlPullParserException e) {
-      Log.e(TAG, "Unable to read config.xml.", e);
-    } catch (IOException ioe) {
-      Log.e(TAG, "Unable to open config.xml.", ioe);
-    }
 
-    if (!senderIdSet) {
-      try {
-        builder.setSenderId(FirebaseApp.getInstance().getOptions().getGcmSenderId());
-      } catch (Exception e) {
-        Log.e(TAG,
-            "Unable to retrieve sender id.  Push messages will not work for Marketing Cloud.", e);
-      }
-    }
+        if (!senderIdSet) {
+            try {
+                builder.setSenderId(FirebaseApp.getInstance().getOptions().getGcmSenderId());
+            } catch (Exception e) {
+                Log.e(TAG,
+                        "Unable to retrieve sender id.  Push messages will not work for Marketing Cloud.", e);
+            }
+        }
 
-    return builder;
-  }
+        return builder;
+    }
 }
